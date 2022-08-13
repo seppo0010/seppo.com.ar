5394b92d49b28dea2bafb75e97bcbe1cad404fa8
Sebastian Waisbrot <seppo0010@gmail.com>
2022-08-13 19:05:02 -0300

    First raw version

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
 import './App.css';
+import projects from './projects.json';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import TelegramIcon from '@mui/icons-material/Telegram';
 
 function App() {
   return (
-    <div>
-      Hello World
-    </div>
+    <Container maxWidth="lg">
+      <Typography variant="h1">
+        Portfolio de Sebastian Waisbrot
+      </Typography>
+      {projects.map((p) => (
+        <Paper key={p.name}>
+          <Box className="project">
+            <div className="projectHeader">
+              <Typography variant="h2">{p.name}</Typography>
+              {p.repository && <a href={p.repository} target="_blank" rel="noreferrer"><GitHubIcon /></a>}
+              {p.url && <a href={p.url} target="_blank" rel="noreferrer"><OpenInNewIcon /></a>}
+              {p.telegram && <a href={p.telegram} target="_blank" rel="noreferrer"><TelegramIcon /></a>}
+            </div>
+            <p>{p.description}</p>
+          </Box>
+        </Paper>
+      ))}
+    </Container>
   );
 }
 
