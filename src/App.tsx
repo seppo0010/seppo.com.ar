5f151cdb976b27ec88ced6c2b68178fdee01d75e
Sebastian Waisbrot <seppo0010@gmail.com>
2022-08-18 08:31:14 -0300

    Display tags

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Box from '@mui/material/Box';
+import Chip from '@mui/material/Chip';
 import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import './App.css';
 import projects from './projects.json';
@@ -19,7 +21,12 @@ function App() {
         <Paper key={p.name}>
           <Box className="project">
             <div className="projectHeader">
-              <Typography variant="h2">{p.name}</Typography>
+              <Typography variant="h2">
+                {p.name}
+              </Typography>
+              {p.tags.length > 0 && <Stack direction="row" spacing={1}>
+                {p.tags.map((tag) => <Chip key={tag} label={tag} />)}
+              </Stack>}
               {p.repository && <a href={p.repository} target="_blank" rel="noreferrer"><GitHubIcon /></a>}
               {p.url && <a href={p.url} target="_blank" rel="noreferrer"><OpenInNewIcon /></a>}
               {p.telegram && <a href={p.telegram} target="_blank" rel="noreferrer"><TelegramIcon /></a>}
